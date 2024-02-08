e20185a7e2a4b66e1302ec617df7bbb06dd279dd
AyushiPohekar <ayushi2021.29@gmail.com>
2024-02-08 11:23:58 +0530

    done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className=' navbar_main'>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Legacies AI</a>
+    <a className="navbar-brand" href="/">Legacies AI</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
